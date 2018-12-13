4fe475f54b5790c2c046f4c0fb074a29e7188473
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-12-13 14:55:52 +0000

    add @roast property to Product class

--- models/product.rb
+++ models/product.rb
@@ -1,11 +1,12 @@
 class Product
 
-  attr_reader :name, :producer_id, :origin
+  attr_reader :name, :producer_id, :origin, :roast
 
   def initialize(options)
     @name = options[:name]
     @producer_id = options[:producer_id].to_i
     @origin = options[:origin]
+    @roast = options[:roast]
   end
 
 end
--- models/specs/product_spec.rb
+++ models/specs/product_spec.rb
@@ -5,7 +5,7 @@ require_relative('../product')
 class ProductTest < MiniTest::Test
 
   def setup
-    options = {name:"Colombian Supremo", producer_id:1, origin:"Colombia"}
+    options = {name:"Colombian Supremo", producer_id:1, origin:"Colombia", roast:"Dark"}
     @product1 = Product.new(options)
   end
 
@@ -19,6 +19,10 @@ class ProductTest < MiniTest::Test
 
   def test_product_origin
     assert_equal("Colombia", @product1.origin)
+  end
+
+  def test_product_roast
+    assert_equal("Dark", @product1.roast)
   end
 
 end
