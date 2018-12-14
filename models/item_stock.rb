2bfcfe289109c38e88548155909ace22eeed4d52
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-12-14 09:35:47 +0000

    Add @product_name to ItemStock

--- models/item_stock.rb
+++ models/item_stock.rb
@@ -1,9 +1,10 @@
 class ItemStock
 
-  attr_reader :product_id
+  attr_reader :product_id, :product_name
 
   def initialize(options)
     @product_id = options[:product_id] #integer
+    @product_name = options[:product_name] #string
   end
 
 end
--- models/specs/item_stock_spec.rb
+++ models/specs/item_stock_spec.rb
@@ -5,12 +5,16 @@ require_relative('../item_stock')
 class ItemStockTest < MiniTest::Test
 
   def setup
-    options = {product_id: 1}
+    options = {product_id: 1, product_name: "Colombian Supremo"}
     @itemstock1 = ItemStock.new(options)
   end
 
   def test_item_stock_product_id
     assert_equal(1, @itemstock1.product_id)
+  end
+
+  def test_item_stock_product_name
+    assert_equal("Colombian Supremo", @itemstock1.product_name)
   end
 
 end
