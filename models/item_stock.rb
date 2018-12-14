5abc8f56b51ca49d208b239b8f9ce1ab064750c0
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-12-14 09:37:44 +0000

    Add @units_in_stock to ItemStock

--- models/item_stock.rb
+++ models/item_stock.rb
@@ -1,10 +1,11 @@
 class ItemStock
 
-  attr_reader :product_id, :product_name
+  attr_reader :product_id, :product_name, :units_in_stock
 
   def initialize(options)
     @product_id = options[:product_id] #integer
     @product_name = options[:product_name] #string
+    @units_in_stock = options[:units_in_stock]
   end
 
 end
--- models/specs/item_stock_spec.rb
+++ models/specs/item_stock_spec.rb
@@ -5,7 +5,7 @@ require_relative('../item_stock')
 class ItemStockTest < MiniTest::Test
 
   def setup
-    options = {product_id: 1, product_name: "Colombian Supremo"}
+    options = {product_id: 1, product_name: "Colombian Supremo", units_in_stock: 10}
     @itemstock1 = ItemStock.new(options)
   end
 
@@ -15,6 +15,10 @@ class ItemStockTest < MiniTest::Test
 
   def test_item_stock_product_name
     assert_equal("Colombian Supremo", @itemstock1.product_name)
+  end
+
+  def test_item_stock_units_in_stock
+    assert_equal(10, @itemstock1.units_in_stock)
   end
 
 end
