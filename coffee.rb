4a5449c27b1b9ab5b5cc9c490c635f6b10e288da
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-12-17 11:12:19 +0000

    Redo products/index view to represent new table structure

--- coffee.rb
+++ coffee.rb
@@ -2,7 +2,6 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('controllers/producers_controller')
 require_relative('controllers/products_controller')
-require_relative('controllers/stock_items_controller')
 
 get '/' do
   erb( :index )
