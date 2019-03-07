4db1e8c08ccf95212f2e4050ae7fe2e7abb8c3b6
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-03-07 19:34:55 +0000

    setup Sinatra for Heroku

--- coffee.rb
+++ coffee.rb
@@ -4,7 +4,9 @@ require_relative('controllers/producers_controller')
 require_relative('controllers/products_controller')
 require_relative('controllers/settings_controller')
 
-get '/' do
-  @products = Product.all
-  erb( :index )
+class App < Sinatra::Base
+  get '/' do
+    @products = Product.all
+    erb( :index )
+  end
 end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require "./coffee"
+run App
